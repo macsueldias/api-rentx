9c14c86da8080d12e44850dba7475bb8258b1b02
Macsuel Dias <macsueldias@hotmail.com>
2023-11-17 12:19:28 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,18 +1,19 @@
 import "reflect-metadata";
 import "dotenv/config";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
-import createConnetion from "@shared/infra/typeorm";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "@shared/infra/http/routes";
 import upload from "@config/upload";
 
-createConnetion();
+createConnection();
 const app = express();
 
 app.use(express.json());
@@ -22,6 +23,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
